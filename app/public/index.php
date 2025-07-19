0e1aee9ecaa11c5c78f041ccf8c262ca85d1da50
Earl Clyde Bañez <earlclyde.mbanez@gmail.com>
2025-07-19 19:25:37 +0800

    hero is here

--- app/public/index.php
+++ app/public/index.php
@@ -6,6 +6,18 @@ switch ($page) {
     case 'signup':
         include '../templates/signin.php';
         break;
+    case 'hero':
+        include '../templates/hero.php';
+        break;
+    case 'dashboard':
+        include '../templates/dashboard.php';
+        break;
+    case 'settings':
+        include '../templates/settings.php';
+        break;
+    case 'logout':
+        include '../templates/logout.php';
+        break;
     default:
         include '../templates/login.php';
         break;
