454f02d38f7eb797d1c2d80cceac568e918e26d3
Earl Clyde <earlclyde.mbanez@gmail.com>
2025-07-22 12:58:51 +0800

    Initial design for Log In

--- app/includes/header.php
+++ app/includes/header.php
@@ -7,16 +7,3 @@
     <link rel="stylesheet" href="../assets/css/base.css">
     <link rel="stylesheet" href="../assets/css/login.css">
 </head>
-<body>
-    <div class="header">
-        <div class="logo">
-            <div class="logo-icon">∞</div>
-            <span>HEPC</span>
-        </div>
-        <nav class="nav">
-            <a href="#" class="nav-link">HOME</a>
-            <a href="#" class="nav-link">ABOUT US</a>
-            <a href="#" class="nav-link">CONTACT</a>
-            <a href="#" class="nav-link active">LOG IN</a>
-        </nav>
-    </div>
