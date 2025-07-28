e6f4b3f5006013fcb8dfd5d4bf5827884943af2c
Earl Clyde <earlclyde.mbanez@gmail.com>
2025-07-28 09:39:32 +0800

    added routing

--- app/views/fileupload_tk_admin.php
+++ app/views/fileupload_tk_admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SOMS - File Upload </title>
-    <link rel="stylesheet" href="../assets/css/soms_fileupload_tk_admin.css">
+    <link rel="stylesheet" href="../../public/assets/css/fileupload_tk_admin.css">
 </head>
 <body>
     <section class="upload-section" id="upload">
