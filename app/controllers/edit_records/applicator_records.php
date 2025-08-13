4a22826744d8a0afb7c4db395f97cf0d11f2cc27
RenzArcilla <renzarcilla09@gmail.com>
2025-08-13 11:12:07 +0800

    controller logic for applicator1 cases a,b

--- app/controllers/edit_records/applicator_records.php
+++ app/controllers/edit_records/applicator_records.php
@@ -1,6 +1,6 @@
 <?php
 
-// Pseudocode 
+// For app1
 // case a: same applicator, different output
     # update applicator_outputs (palitan output) === DONE 
     # update cumulative (either dagdag or bawas) === NOT YET
@@ -9,7 +9,7 @@
     # decrement monitor_applicator ng previous output === NOT YET
     # increment monitor_applicator ng bagong output === NOT YET
 
-// Check for case for app2 
+// For app2 
 // case a: same applicator, different output 
     # update applicator_outputs (palitan output) === DONE
     # update cumulative (either dagdag or bawas) === NOT YET
@@ -24,7 +24,24 @@
     # remove previous app2 output in applicator_outputs === NOT YET
     # decrement monitor_applicator === NOT YET
 
-// ===== APPLICATOR 1 =====
+
+
+    // ===== Common operations regardless of case ===== 
+$update_app1_output_result = updateApplicatorOutput($app1_data,
+        $app1_output, $record_id, $prev_app1_data);
+if (is_string($update_app1_output_result)) {
+    throw new Exception($update_app1_output_result);
+}
+
+if (!empty($app2)) {
+    $update_app2_output_result = updateApplicatorOutput($app2_data,
+        $app2_output, $record_id, $prev_app2_data);
+    if (is_string($update_app2_output_result)) {
+        throw new Exception($update_app2_output_result);
+    }
+}
+
+// APPLICATOR 1: Check for edit case
 $app1_case = null;
 if ($app1 === $prev_app1 && $app1_output !== $prev_app1_output) {
     $app1_case = "A"; // same applicator, different output
@@ -32,7 +49,7 @@ if ($app1 === $prev_app1 && $app1_output !== $prev_app1_output) {
     $app1_case = "B"; // different applicator
 }
 
-// ===== APPLICATOR 2 =====
+// APPLICATOR 2: Check for edit case
 $app2_case = null;
 if ($app2 === $prev_app2 && $app2_output !== $prev_app2_output) {
     $app2_case = "A"; // same applicator, different output
@@ -44,40 +61,79 @@ if ($app2 === $prev_app2 && $app2_output !== $prev_app2_output) {
     $app2_case = "D"; // old → none
 }
 
-// ===== APPLICATOR 1 =====
+
+
+// APPLICATOR 1 
 switch ($app1_case) {
     case "A": // same applicator, different output
-        # update applicator_outputs (palitan output) === DONE 
-        # update cumulative (either dagdag or bawas) === NOT YET
+        # update applicator_outputs (palitan output)
+            $update_app1_output_result = updateApplicatorOutput($app1_data,
+                                    $app1_output, $record_id, $prev_app1_data);
+            if (is_string($update_app1_output_result)) {
+                throw new Exception($update_app1_output_result);
+            }
+        # update cumulative (either dagdag or bawas)
+            $direction = "increment";
+            $applicator_output = $app1_output - $prev_app1_output;
+            if ($applicator_output < 0 ) {
+                $direction = "decrement";
+            }
+            $result = monitorApplicatorOutput($app1_data, $applicator_output, $direction);
+            if (is_string($result)) {
+                throw new Exception($result);
+            }
         break;
 
     case "B":
-        # update old app1 output to new app1 output in applicator_outputs === DONE
+        # update old app1 output to new app1 output in applicator_outputs
+            $update_app1_output_result = updateApplicatorOutput($app1_data,
+                                    $app1_output, $record_id, $prev_app1_data);
+            if (is_string($update_app1_output_result)) {
+                throw new Exception($update_app1_output_result);
+            }
         # decrement monitor_applicator ng previous output === NOT YET
+            $result = monitorApplicatorOutput($prev_app1_data, $prev_app1_output, "decrement");
+            if (is_string($result)) {
+                throw new Exception($result);
+            }
         # increment monitor_applicator ng bagong output === NOT YET
+            $result = monitorApplicatorOutput($app1_data, $app1_output, "increment");
+            if (is_string($result)) {
+                throw new Exception($result);
+            }
         break;
 } 
 
 // ===== APPLICATOR 2 =====
 switch ($app2_case) {
     case "A": // same applicator, different output
-        # update applicator_outputs (palitan output) === DONE 
-        # update cumulative (either dagdag or bawas) === NOT YET
+        # update applicator_outputs (palitan output)
+            $update_app2_output_result = updateApplicatorOutput($app2_data,
+                                    $app2_output, $record_id, $prev_app2_data);
+            if (is_string($update_app2_output_result)) {
+                throw new Exception($update_app2_output_result);
+            }
+        # update cumulative (either dagdag or bawas)
         break;
 
     case "B":
-        # update old app1 output to new app1 output in applicator_outputs === DONE
-        # decrement monitor_applicator ng previous output === NOT YET
-        # increment monitor_applicator ng bagong output === NOT YET
+        # update old app1 output to new app1 output in applicator_outputs
+            $update_app2_output_result = updateApplicatorOutput($app2_data,
+                                    $app2_output, $record_id, $prev_app2_data);
+            if (is_string($update_app2_output_result)) {
+                throw new Exception($update_app2_output_result);
+            }
+        # decrement monitor_applicator ng previous output
+        # increment monitor_applicator ng bagong output
         break;
 
     case "C": // same applicator, different output
         # create new applicator_output
-        # increment monitor_applicator for new app2 === NOT YET
+        # increment monitor_applicator for new app2
         break;
 
     case "D":
-        # remove previous app2 output in applicator_outputs === NOT YET
-        # decrement monitor_applicator === NOT YET
+        # remove previous app2 output in applicator_outputs
+        # decrement monitor_applicator
         break;
 } 
