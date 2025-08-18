e8e95f134fe3afca2c71f0f121ba7ad775342f52
EarlClydeeee <earlclyde.mbanez@gmail.com>
2025-08-18 09:04:50 +0800

    fix: closing add modal via clicking outside

--- public/assets/js/record_output.js
+++ public/assets/js/record_output.js
@@ -6,3 +6,10 @@ function openModal() {
 function closeModal() {
     document.getElementById('modalOverlay').classList.remove('active');
 }
+
+// Close modal when clicking outside
+document.getElementById('modalOverlay').addEventListener('click', function(e) {
+    if (e.target === this) {
+        closeModal();
+    }
+});
