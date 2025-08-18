a6656714c8728c0516be3ae6ffee002f43adf686
RenzArcilla <163639755+RenzArcilla@users.noreply.github.com>
2025-08-18 19:56:07 +0800

    model for getting applicator-reset data on timestamp

--- app/models/read_applicator_reset.php
+++ app/models/read_applicator_reset.php
@@ -34,3 +34,49 @@ function getApplicatorReset($applicator_id, $part_name) {
 
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+
+
+function getApplicatorResetOnTimeStamp($applicator_id, $part_name, $reset_time) {
+    /*
+    Retrieve an applicator_reset records for a specific applicator and part.
+
+    Args:
+    - $applicator_id : int, 
+    - $part_name: string, name of applicator part
+    - $reset_time: time of reset
+
+    Returns:
+    - array: associative array of applicator_reset records (empty array if none)
+    - string: contains error message
+    */
+
+
+    try {
+        // Prepare SQL select query with first_name and last_name
+        $stmt = $pdo->prepare("
+            SELECT * 
+            FROM applicator_reset 
+            WHERE applicator_id = :applicator_id
+                AND part_reset = :part_name
+                AND reset_time = :reset_time");
+
+        // Bind parameters
+        $stmt->bindParam(':applicator_id', $applicator_id);
+        $stmt->bindParam(':part_name', $part_name);
+        $stmt->bindParam(':reset_time', $reset_time);
+
+        // Execute the query
+        $stmt->execute();
+
+        // Fetch user data
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $data;
+
+    } catch (PDOException $e) {
+        // Log error and return an error message on failure
+        error_log("Database Error: " . $e->getMessage());
+        return "Database error occurred: " . htmlspecialchars($e->getMessage(), ENT_QUOTES);
+    }
+}
