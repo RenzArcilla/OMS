f551c65386529dd614bbfa940eca7ead4d407a66
EarlClydeeee <earlclyde.mbanez@gmail.com>
2025-08-18 09:03:00 +0800

    fix: add record modal

---
+++ public/assets/js/record_output.js
@@ -0,0 +1,8 @@
+// Modal functions
+function openModal() {
+    document.getElementById('modalOverlay').classList.add('active');
+}
+
+function closeModal() {
+    document.getElementById('modalOverlay').classList.remove('active');
+}
