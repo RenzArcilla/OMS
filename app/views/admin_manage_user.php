52f18df070dbcab0471033c8fb05683bfd1bb24d
EarlClydeeee <earlclyde.mbanez@gmail.com>
2025-08-20 14:09:41 +0800

    fix: button sizes of confirmation delete

--- app/views/admin_manage_user.php
+++ app/views/admin_manage_user.php
@@ -503,7 +503,7 @@
                 </div>
             </div>
 
-            <div class="action-buttons">
+            <div class="action-buttons-to-confirm">
                 <a href="#" class="btn btn-primary" id="pageDeleteBtn" onclick="return confirmPageDeletion(event)">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
