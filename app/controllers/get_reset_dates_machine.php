4f25d1569d5d27e10875b0358db877c19e28bed3
RenzArcilla <renzarcilla09@gmail.com>
2025-08-20 10:52:33 +0800

    feat(machine-undo-reset-modal): add controller to fetch reset timestamps via AJAX

---
+++ app/controllers/get_reset_dates_machine.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    This file will be used to display available timestamps for machine part output reset via AJAX.
+*/
+
+
+require_once '../models/read_machine_reset.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $part_name = $_POST['part_name'] ?? null;
+    $machine_id = $_POST['machine_id'] ?? null;
+
+    if (!$part_name || !$machine_id) {
+        echo json_encode([]);
+        exit;
+    }
+
+    $resets = getMachineReset($machine_id, $part_name);
+    echo json_encode($resets);
+}
