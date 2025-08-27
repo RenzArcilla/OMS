e12c5c77cc409e67c6daf046542e9d2e1897311f
EarlClydeeee <earlclyde.mbanez@gmail.com>
2025-08-27 13:14:09 +0800

    fix: remove container and admin main content at dashboard_machine.css, using the base.css now

--- app/views/add_entry.php
+++ app/views/add_entry.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['user_id'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Add Machine or Applicator</title>4
+    <title>Add Machine or Applicator</title>
     <link rel="stylesheet" href="../../public/assets/css/base/base.css">
     <link rel="stylesheet" href="../../public/assets/css/add_entry.css">
     <link rel="stylesheet" href="/SOMS/public/assets/css/components/modal.css">
