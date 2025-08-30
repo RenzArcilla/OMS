aa0d039810cefb0d41e9897342e8e5b7dd9da487
Earl Clyde Bañez <earlclyde.mbanez@gmail.com>
2025-08-30 08:52:22 +0800

    fix: bug at js for js-alert

--- public/assets/js/js_alert.js
+++
@@ -1,8 +0,0 @@
-function closeJsAlertModal() {
-    document.getElementById('jsAlertModalOverlay').remove();
-    window.location.href = '$redirect_url';
-}
-// Allow pressing Enter to close modal
-document.addEventListener('keydown', function(e) {
-    if (e.key === 'Enter') closeJsAlertModal();
-});
